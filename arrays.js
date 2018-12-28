18b65c9647e1c294d25adb584d65b6530149e48d
Paul Riverain <paulriverain@yahoo.com>
2018-12-28 19:06:53 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  array.unshift(element)
+}
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+function addElementToEndOfArray(array, element){
+  return [array..., element]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
